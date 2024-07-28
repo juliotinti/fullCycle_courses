ae4c39678c702126e6f1151a351ef9ceef52e2bf
julioTinti <julio_tinti_andrade@hotmail.com>
2024-07-28 20:04:21 -0300

    Value objects lecture

--- DDD/src/entities/customer.ts
+++ DDD/src/entities/customer.ts
@@ -1,13 +1,26 @@
+import Address from "./address";
+
 class Customer {
     _id: string;
     _name: string; 
-    _address: string;
-    _active: boolean = true;
+    _address!: Address;
+    _active: boolean = false;
 
     constructor(id: string, name:string, address: string){
         this._id = id
         this._name = name
-        this._address = address
+        this.validate();
+    }
+
+    validate() {
+        if (this._name.length === 0)
+        {
+            throw new Error("Name is required");
+        }
+        if (this._id.length === 0)
+        {
+            throw new Error("Name is required");
+        }
     }
 
     changeName(name: string){
@@ -15,11 +28,18 @@ class Customer {
     }
 
     activate() {
+        if(this._address === undefined){
+            throw new Error("Address is mandatory to activate a customer");
+        }
         this._active = true;
     }
 
     deactivate() {
         this._active = false;
+    }
+
+    set Address(address: Address){
+        this._address = address;
     }
 
 }
