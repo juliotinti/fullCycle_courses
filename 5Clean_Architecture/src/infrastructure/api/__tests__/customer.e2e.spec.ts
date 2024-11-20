c9d84311e5f018d06852575fb1010fcf0397f813
julioTinti <julio_tinti_andrade@hotmail.com>
2024-11-20 18:15:27 -0300

    API Challenge

---
+++ 5Clean_Architecture/src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -0,0 +1,78 @@
+import { app, sequelize } from '../express';
+import request from 'supertest';
+
+describe('E2E test for customer', () => {
+    beforeEach(async () => {
+        await sequelize.sync({ force: true });
+    });
+
+    afterAll(async () => {
+        await sequelize.close();
+    });
+
+    it('should create a customer', async () => {
+        const response = await request(app)
+            .post('/customer')
+            .send({
+                name: 'John',
+                address: {
+                    street: 'Street',
+                    city: 'City',
+                    number: 123,
+                    zip: '12345',
+                },
+            });
+
+        expect(response.status).toBe(200);
+        expect(response.body.name).toBe('John');
+        expect(response.body.address.street).toBe('Street');
+        expect(response.body.address.city).toBe('City');
+        expect(response.body.address.number).toBe(123);
+        expect(response.body.address.zip).toBe('12345');
+    });
+
+    it('should not create a customer', async () => {
+        const response = await request(app).post('/customer').send({
+            name: 'John',
+        });
+
+        expect(response.status).toBe(500);
+    });
+
+    it('should list all customers', async () => {
+        const response = await request(app)
+            .post('/customer')
+            .send({
+                name: 'John',
+                address: {
+                    street: 'Street',
+                    city: 'City',
+                    number: 123,
+                    zip: '12345',
+                },
+            });
+
+        const response2 = await request(app)
+            .post('/customer')
+            .send({
+                name: 'John2',
+                address: {
+                    street: 'Street2',
+                    city: 'City2',
+                    number: 1232,
+                    zip: '123452',
+                },
+            });
+
+        const listResponse = await request(app).get('/customer').send();
+
+        expect(listResponse.status).toBe(200);
+        expect(listResponse.body.customers.length).toBe(2);
+        const customer = listResponse.body.customers[0];
+        expect(customer.name).toBe('John');
+        expect(customer.address.street).toBe('Street');
+        const customer2 = listResponse.body.customers[1];
+        expect(customer2.name).toBe('John2');
+        expect(customer2.address.street).toBe('Street2');
+    });
+});
---
+++ 5Clean_Architecture/src/infrastructure/api/routes/customer.route.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from 'express';
+import CreateCustomerUseCase from '../../../usecase/customer/create/create.customer.usecase';
+import CustomerRepository from '../../customer/repository/sequelize/customer.repository';
+import Address from '../../../domain/customer/value-object/address';
+import ListCustomerUseCase from '../../../usecase/customer/list/list.customer.usecase';
+import Customer from '../../../domain/customer/entity/customer';
+
+export const customerRoute = express.Router();
+
+customerRoute.post('/', async (req: Request, res: Response) => {
+    const useCase = new CreateCustomerUseCase(new CustomerRepository());
+    try {
+        const customerDto = {
+            name: req.body.name,
+            address: {
+                street: req.body.address.street,
+                city: req.body.address.city,
+                number: req.body.address.number,
+                zip: req.body.address.zip,
+            },
+        };
+
+        const output = await useCase.execute(customerDto);
+        res.send(output);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+customerRoute.get('/', async (req: Request, res: Response) => {
+    const useCase = new ListCustomerUseCase(new CustomerRepository());
+    try {
+        const output = await useCase.execute({});
+        res.send(output);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
