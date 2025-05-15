4dc43a617b4deb68db1d96c1d24ba901456e124d
julioTinti <julio_tinti_andrade@hotmail.com>
2025-05-14 23:27:51 -0300

    WIP

---
+++ 7Microservices/WalletCore/internal/usecase/create_client/create_client.go
@@ -0,0 +1,50 @@
+package createclient
+
+import (
+	"time"
+
+	"github.com/juliotinti/fullCycle_courses/internal/entity"
+	"github.com/juliotinti/fullCycle_courses/internal/gateway"
+)
+
+type CreateClientInputDTO struct {
+	Name  string
+	Email string
+}
+
+type CreateClientOutputDTO struct {
+	ID        string
+	Name      string
+	Email     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type CreateClientUseCase struct {
+	ClientGateway gateway.ClientGateway
+}
+
+// é tipo um construtor do orientada a objetos
+func NewCreateClientUseCase(clientGateway gateway.ClientGateway) *CreateClientUseCase {
+	return &CreateClientUseCase{
+		ClientGateway: clientGateway,
+	}
+}
+
+func (c *CreateClientUseCase) Execute(input CreateClientInputDTO) (*CreateClientOutputDTO, error) {
+	client, err := entity.NewClient(input.Name, input.Email)
+	if err != nil {
+		return nil, err
+	}
+	err = c.ClientGateway.Save(client)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateClientOutputDTO{
+		ID:        client.ID,
+		Name:      client.Name,
+		Email:     client.Email,
+		CreatedAt: client.CreatedAt,
+		UpdatedAt: client.UpdatedAt,
+	}, nil
+}
